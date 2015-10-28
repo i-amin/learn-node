365643f123a125fb2c73d993faf8fe1c81c5d9a3
ibrahem amin <ibrahem.m.amin@gmail.com>
2015-10-28 14:47:51 +0200

    [walking skeleton] adding stylus

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
-var express = require('express');
+var express = require('express'),
+    stylus = require('stylus'),
+    logger = require('morgan'),
+    bodyParser = require('body-parser');
 
 
 // set the environment variable to dev or production
@@ -10,10 +13,27 @@ var app = express();
 // configure the express server
 app.set('views', __dirname + '/server/views');
 app.set('view engine', 'jade');
+app.use(logger('dev')); // logging system for node project
+app.use(bodyParser.urlencoded({
+    extended: true
+})); // request parser
 
 app.get('*', function (req, res) {
     res.render('index');
 });
+
+// stylus configuration
+function compile(str, path) {
+    //return
+}
+app.use(stylus.middleware({
+    src: __dirname + '/public',
+    compile: compile
+}));
+
+
+// static files middleware
+app.use(express.static(__dirname + '/public'));
 
 var port = 3000;
 app.listen(port);
